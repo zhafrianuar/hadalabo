e893ab59f4092dfbb4be373af7b76be654593960
whei2419 <joelnacionales24@gmail.com>
2024-05-10 03:42:55 +0800

    fix ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
--- resources/views/station.blade.php
+++ resources/views/station.blade.php
@@ -3,25 +3,26 @@
 @section('content')
 
 
-<div class="container-fluid py-5 home">
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center align-items-center">
-                <div class="branding-container">
-                    <img class="logo" src="{{ asset('images/logo.svg') }}" alt="brand-logo">
-                </div>
-
-                </div>
-                <div class="col-12 mt-3 text-content text-center">
-                    <h1 class="heading mt-5">Station {{$station->id}}</h1>
-                    <h2 class="heading mt-2">{{$station->name}}</h2>
-
-                     
-                    <img class="subheading mt-5" src="{{ asset('images/' . $station->id . '.svg') }}" alt="Station Image">
-                    <br>
-
-                    <img class="subheading mt-5" src="{{ asset('images/camera.svg') }}" alt="Station Image">
-                    <p class="already-register">Scan the QR code at the station to proceed</p>
-                </div>
+<div class="container-fluid py-5 station">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <div class="branding-container">
+                <img class="logo" src="{{ asset('images/logo.svg') }}" alt="brand-logo">
             </div>
         </div>
+        <div class="col-12 mt-3 text-content text-center">
+            <div class="icon-container mt-4">
+                <img class="icon-bg" src="{{ asset('images/icon-bg.svg') }}" alt="Lock Image">
+                <img class="icon" src="{{ asset('images/station3icon.svg') }}" alt="Lock Image">
+            </div>
+            <h1 class="station-heading mt-2">Station {{$station->id}}</h1>
+            <h2 class="station-subheading">{{$station->name}}</h2>
+            <img class="mt-5" src="{{ asset('images/' . $station->id . '.svg') }}" alt="Station Image">
+            <a href="" class="camera-btn mx-auto mt-4"><img  src="{{ asset('images/camera.svg') }}" alt=""></a>
+            <p class="bottom-text mt-2">Scan the QR code at the station to proceed</p>
+        </div>
+    </div>
+</div>
 @endsection
+
+
